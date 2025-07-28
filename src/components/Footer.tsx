62cc0f2ef9d3967c0c05bf3942dad95b6e4816c8
Abhishek Kumar <110527862+abhishek326kr@users.noreply.github.com>
2025-07-28 19:42:18 +0530

    Fix missing comma in Footer.tsx interface

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer: React.FC<FooterProps> = ({
   onNavigateToUserManual,
   onNavigateToBlog,
   onNavigateToCommunity,
-  onNavigateToSupport
+  onNavigateToSupport: () => void;
   onNavigateToPrivacyPolicy,
   onNavigateToTermsConditions,
   onNavigateToReturnPolicy,
