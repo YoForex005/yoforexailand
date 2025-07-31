1b95eb966eacc020761e13d94ff3e50427544a4a
Abhishek Kumar <110527862+abhishek326kr@users.noreply.github.com>
2025-07-31 18:21:45 +0530

    Fix process is not defined error in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {},
+    global: 'globalThis'
+  },
+  resolve: {
+    alias: {
+      process: 'process/browser'
+    }
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['next/router']
   },
 });
